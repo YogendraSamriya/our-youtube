c27d13f9869e7c6e0840fda9483d98a5a99e026e
YogendraSamriya <ysamriya4774@gmail.com>
2026-02-23 04:46:28 +0530

    Update 23-02-2026

--- our-youtube/src/components/Navbar.jsx
+++ our-youtube/src/components/Navbar.jsx
@@ -1,25 +1,45 @@
-
+import { Search, Mic, Plus, Bell, User } from "lucide-react";
 import "../styles/navbar.css"
 
 function Navbar({ toggleSidebar }) {
   return (
     <header className="navbar">
       
-      <button className="menu-btn" onClick={toggleSidebar}>
-        ☰
-      </button>
+      <div className="left">
+        <button className="menu-btn" onClick={toggleSidebar}>
+          ☰
+         </button>
 
-      <div className="logo">OurYouTube</div>
+         <div className="logo">OurYouTube</div>
+      </div>
 
-      <input 
-        type="text"
-        placeholder="Search"
-        className="search-bar"
-      />
+      <div className="nav-center">
+        <div className="search-container">
+          <input type="text" placeholder="Search" className="search-bar" />
+          <button className="search-btn">
+             <Search size={18} />
+          </button>
+        </div>
+
+        <button className="mic-btn">
+          <Mic size={20} />
+        </button>
+      </div>
 
       <div className="nav-actions">
-        <button>Upload</button>
-        <button>Login</button>
+        <button className="create-btn">
+          <Plus size={18} />
+          <span>Create</span>
+        </button>
+
+        <button className="icon-btn notification-btn">
+          <Bell size={20} />
+          <span className="badge">9+</span>
+        </button>
+
+        <button className="icon-btn">
+          <User size={20} />
+        </button>
       </div>
     </header>
   )
